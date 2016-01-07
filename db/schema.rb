3b3037e85e15ce5039b6e35ab8acfee4bbef4654
jsnowe <j.e.snowe@gmail.com>
2016-01-07 00:58:19 +0000

    create chefs and recipes association and tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160105034611) do
+ActiveRecord::Schema.define(version: 20160107003313) do
 
   create_table "chefs", force: true do |t|
     t.string   "chefname"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160105034611) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "chef_id"
   end
 
 end
